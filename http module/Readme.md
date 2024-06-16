14c60c48ff106b9512f5b5f709aebf9657ae4a12
Satyarshi Shukla <satyarshishukla123@gmail.com>
2024-06-16 06:45:37 +0530

    http module

---
+++ http module/Readme.md
@@ -0,0 +1,6 @@
+# HTTP - HyperText Transfer Protocol
+
+- It is the protocol for transmitting data over internet
+- Http defines how client and servers communicate with each other
+- It also defines how responds and requests should be formatted and transmitted
+- It is the foundation of data communication for the world wide web or the internet
---
+++ http module/index.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("<h1>Hello World</h1>");
+  } else if (req.url === "/about") {
+    res.write("<h1>About Page</h1>");
+  } else {
+    res.write("<h1>Page Not Found</h1>");
+  }
+});
+
+server.listen(3000);
+console.log("Server is listening on port 3000");
